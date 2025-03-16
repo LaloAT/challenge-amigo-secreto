b7e3dc1b2d5b96d5d52266fd5fd639a50ea951de
LaloAT <ayalatamayo.je@gmail.com>
2025-03-15 21:49:36 -0600

    feat: agregar validaciones y funcionalidad para el sorteo de amigo secreto

--- app.js
+++ app.js
@@ -1,1 +1,45 @@
-// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let amigos = [];
+
+function agregarAmigo() {
+    let input = document.getElementById("amigo");
+    let nombre = input.value.trim(); 
+
+    if (nombre === "") {
+        alert("Por favor, inserte un nombre.");
+        return; 
+    }
+
+    amigos.push(nombre);
+
+    input.value = "";
+
+    actualizarListaAmigos();
+}
+
+function actualizarListaAmigos() {
+
+    let lista = document.getElementById("listaAmigos");
+
+    lista.innerHTML = "";
+
+    for (let i = 0; i < amigos.length; i++) {
+        let li = document.createElement("li"); 
+        li.textContent = amigos[i];
+        lista.appendChild(li); 
+    }
+}
+
+function sortearAmigo() {
+    if (amigos.length === 0) {
+        alert("La lista está vacía. Agrega al menos un amigo antes de sortear.");
+        return; 
+    }
+
+    let indiceAleatorio = Math.floor(Math.random() * amigos.length);
+
+    let amigoSorteado = amigos[indiceAleatorio];
+
+    let resultado = document.getElementById("resultado");
+    resultado.innerHTML = `<li>🎉 Amigo secreto: <strong>${amigoSorteado}</strong> 🎁</li>`;
+}
+
